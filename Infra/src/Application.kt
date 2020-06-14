b48316889978ab30c135cd3b0586b3915924f2f0
awataki <awataki1206@gmail.com>
2020-06-14 19:36:37 +0900

    initial commit

---
+++ Infra/src/Application.kt
@@ -0,0 +1,67 @@
+package sh.awtk
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.application.log
+import io.ktor.auth.Authentication
+import io.ktor.features.CORS
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.StatusPages
+import io.ktor.gson.gson
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.locations.Locations
+import io.ktor.response.respond
+import io.ktor.routing.routing
+import sh.awtk.vothemis.exception.HttpException
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(Locations) {
+    }
+
+    install(CORS) {
+        method(HttpMethod.Options)
+        method(HttpMethod.Put)
+        method(HttpMethod.Delete)
+        method(HttpMethod.Patch)
+        header(HttpHeaders.Authorization)
+        allowCredentials = true
+        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+    }
+
+    install(Authentication) {
+    }
+
+    install(ContentNegotiation) {
+        gson()
+    }
+
+    routing {
+
+        install(StatusPages) {
+            exception<HttpException> { e ->
+                log.info(e.errMessage)
+                call.respond(HttpStatusCode.fromValue(e.code))
+            }
+            exception<Throwable> { e ->
+                log.info(e.toString())
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+        }
+
+        Routes().apply {
+            Question()
+            User()
+            Login()
+        }
+    }
+}
+
+
