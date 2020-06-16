362d43c94ce01dd60a242260776284fe33afafd2
awataki <awataki1206@gmail.com>
2020-06-16 18:35:25 +0900

    create user dao

---
+++ External/src/main/kotlin/sh/awtk/vothemis/database/table/UserTable.kt
@@ -0,0 +1,19 @@
+package sh.awtk.vothemis.database.table
+
+import org.jetbrains.exposed.dao.EntityID
+import org.jetbrains.exposed.dao.LongEntity
+import org.jetbrains.exposed.dao.LongEntityClass
+import org.jetbrains.exposed.dao.LongIdTable
+import org.jetbrains.exposed.sql.Column
+
+object UserTable : LongIdTable("users") {
+    val userName: Column<String> = varchar("user_name", 256).uniqueIndex()
+    val hash: Column<String> = varchar("hash", 70)
+}
+
+class UserEntity(id: EntityID<Long>) : LongEntity(id) {
+    companion object : LongEntityClass<UserEntity>(UserTable)
+
+    var name by UserTable.userName
+    var hash by UserTable.hash
+}
