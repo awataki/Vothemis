d045e81aa316c71c306faec4be49480273375db8
awataki <awataki1206@gmail.com>
2020-06-18 18:45:35 +0900

    add question to koin module

--- Infra/src/sh/awtk/vothemis/module/KoinModules.kt
+++ Infra/src/sh/awtk/vothemis/module/KoinModules.kt
@@ -3,15 +3,22 @@ package sh.awtk.vothemis.module
 import org.koin.dsl.module
 import org.koin.experimental.builder.single
 import org.koin.experimental.builder.singleBy
+import sh.awtk.vothemis.exposed.repository.CandidateRepositoryImpl
+import sh.awtk.vothemis.exposed.repository.QuestionRepositoryImpl
 import sh.awtk.vothemis.exposed.repository.TransactionImpl
 import sh.awtk.vothemis.exposed.repository.UserRepositoryImpl
+import sh.awtk.vothemis.interfaces.repository.ICandidateRepository
+import sh.awtk.vothemis.interfaces.repository.IQuestionRepository
 import sh.awtk.vothemis.interfaces.repository.ITransaction
 import sh.awtk.vothemis.interfaces.repository.IUserRepository
 import sh.awtk.vothemis.interfaces.service.IAuthenticationService
+import sh.awtk.vothemis.interfaces.service.IQuestionService
 import sh.awtk.vothemis.interfaces.service.IUserService
 import sh.awtk.vothemis.presenter.LoginPresenter
+import sh.awtk.vothemis.presenter.QuestionPresenter
 import sh.awtk.vothemis.presenter.UserPresenter
 import sh.awtk.vothemis.service.AuthenticationService
+import sh.awtk.vothemis.service.QuestionService
 import sh.awtk.vothemis.service.UserService
 
 class KoinModules {
@@ -20,12 +27,16 @@ class KoinModules {
             // Presenter
             single<UserPresenter>()
             single<LoginPresenter>()
+            single<QuestionPresenter>()
             // Service
             singleBy<IUserService, UserService>()
             singleBy<IAuthenticationService, AuthenticationService>()
+            singleBy<IQuestionService, QuestionService>()
             //Repository
             singleBy<ITransaction, TransactionImpl>()
             singleBy<IUserRepository, UserRepositoryImpl>()
+            singleBy<IQuestionRepository, QuestionRepositoryImpl>()
+            singleBy<ICandidateRepository, CandidateRepositoryImpl>()
         }
     }
 }
