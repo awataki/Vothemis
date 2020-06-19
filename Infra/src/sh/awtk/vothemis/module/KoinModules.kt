5790e11a17bea4d1c34b31436161b2fb771b3cae
awataki <awataki1206@gmail.com>
2020-06-19 16:49:09 +0900

    add voting repository to koin

--- Infra/src/sh/awtk/vothemis/module/KoinModules.kt
+++ Infra/src/sh/awtk/vothemis/module/KoinModules.kt
@@ -3,14 +3,8 @@ package sh.awtk.vothemis.module
 import org.koin.dsl.module
 import org.koin.experimental.builder.single
 import org.koin.experimental.builder.singleBy
-import sh.awtk.vothemis.exposed.repository.CandidateRepositoryImpl
-import sh.awtk.vothemis.exposed.repository.QuestionRepositoryImpl
-import sh.awtk.vothemis.exposed.repository.TransactionImpl
-import sh.awtk.vothemis.exposed.repository.UserRepositoryImpl
-import sh.awtk.vothemis.interfaces.repository.ICandidateRepository
-import sh.awtk.vothemis.interfaces.repository.IQuestionRepository
-import sh.awtk.vothemis.interfaces.repository.ITransaction
-import sh.awtk.vothemis.interfaces.repository.IUserRepository
+import sh.awtk.vothemis.exposed.repository.*
+import sh.awtk.vothemis.interfaces.repository.*
 import sh.awtk.vothemis.interfaces.service.IAuthenticationService
 import sh.awtk.vothemis.interfaces.service.IQuestionService
 import sh.awtk.vothemis.interfaces.service.IUserService
@@ -37,6 +31,7 @@ class KoinModules {
             singleBy<IUserRepository, UserRepositoryImpl>()
             singleBy<IQuestionRepository, QuestionRepositoryImpl>()
             singleBy<ICandidateRepository, CandidateRepositoryImpl>()
+            singleBy<IVotingRepository, VotingRepositoryImpl>()
         }
     }
 }
