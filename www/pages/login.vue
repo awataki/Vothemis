9a77b16e27a32151fbb610f4d406a229c20f3260
awataki <awataki1206@gmail.com>
2020-08-08 18:42:05 +0900

    create login page

---
+++ www/pages/login.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-container
+    class="fill-height"
+    fluid
+  >
+    <v-row
+      align="center"
+      justify="center"
+    >
+      <v-col
+        cols="12"
+        sm="8"
+        md="4"
+      >
+        <v-card class="elevation-12 pa-4">
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="ユーザー名"
+                name="login"
+                prepend-icon="mdi-account"
+                type="text"
+              />
+
+              <v-text-field
+                id="password"
+                label="パスワード"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+              />
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn color="primary" @click="login">
+              ログイン
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  methods: {
+    login () {
+    }
+  }
+})
+</script>
