e2f6c57358fefa47fe3f08ffe276b543e2908721
awataki <awataki1206@gmail.com>
2020-08-16 18:00:04 +0900

    add pass param in user object

--- www/assets/scripts/api/UserAPI.ts
+++ www/assets/scripts/api/UserAPI.ts
@@ -4,17 +4,17 @@ import BaseAPI from '~/assets/scripts/api/BaseAPI'
 
 export default class UserAPI extends BaseAPI {
   async getBy (userId: number): Promise<User> {
-    const res = await axios.get('/api/v1/user/' + userId.toString(), this.createHeader())
+    const res = await axios.get('/api/v1/user/' + userId.toString(), this.createHeaderWithToken())
     return this.parseUser(res.data)
   }
 
   async create (user: User) :Promise<User> {
-    const res = await axios.post('/api/v1/user/create', user, this.createHeaderWithToken())
+    const res = await axios.post('/api/v1/user/create', { name: user.name, password: user.pass, bio: user.bio }, this.createHeaderWithToken())
     return this.parseUser(res.data)
   }
 
   async update (user:User) :Promise<User> {
-    const res = await axios.put('/api/v1/user/' + user.id.toString, this.createHeaderWithToken())
+    const res = await axios.put('/api/v1/user/' + user.id, { name: user.name, password: user.pass, bio: user.bio }, this.createHeaderWithToken())
     return this.parseUser(res.data)
   }
 
@@ -22,6 +22,7 @@ export default class UserAPI extends BaseAPI {
     return new User(
       +json.id,
       json.name,
+      '',
       json.bio
     )
   }
--- www/assets/scripts/model/User.ts
+++ www/assets/scripts/model/User.ts
@@ -15,6 +15,14 @@ export default class User {
     this._name = value
   }
 
+  get pass (): String {
+    return this._pass
+  }
+
+  set pass (value: String) {
+    this._pass = value
+  }
+
   get bio (): String {
     return this._bio
   }
@@ -23,13 +31,15 @@ export default class User {
     this._bio = value
   }
 
-    private _id!:Number;
-    private _name!:String;
-    private _bio!:String;
+  private _id!:Number;
+  private _pass:String = '';
+  private _name!:String;
+  private _bio!:String;
 
-    constructor (id:Number, name:String, bio:String) {
-      this.id = id
-      this.name = name
-      this.bio = bio
-    }
+  constructor (id:Number, name:String, pass:String, bio:String) {
+    this.id = id
+    this.name = name
+    this.pass = pass
+    this.bio = bio
+  }
 }
