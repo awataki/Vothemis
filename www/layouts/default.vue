910586e1d51dc98c958f98c64efaa24fe18da969
awataki <awataki1206@gmail.com>
2020-08-17 16:15:40 +0900

    add user avatar

--- www/layouts/default.vue
+++ www/layouts/default.vue
@@ -35,12 +35,49 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title />
       <v-spacer />
-      <v-switch
-        v-model="$vuetify.theme.dark"
-        prepend-icon="mdi-brightness-6"
-        hide-details
-        inset
-      />
+      <v-menu offset-y :close-on-content-click="false">
+        <template v-slot:activator="{attr,on}">
+          <v-avatar size="40" color="primary" v-bind="attr" v-on="on">
+            <span>{{ avatarText }}</span>
+          </v-avatar>
+        </template>
+        <v-list>
+          <v-list-item @click="$router.push('/profile')">
+            <v-list-item-avatar>
+              <v-avatar size="40" color="primary" style="color: white;" v-bind="attr" v-on="on">
+                <span>{{ avatarText }}</span>
+              </v-avatar>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title>
+                {{ user.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle>
+                {{ user.bio }}
+              </v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-title>
+              ダークテーマ
+            </v-list-item-title>
+            <v-list-item-action>
+              <v-switch
+                v-model="$vuetify.theme.dark"
+                hide-details
+                inset
+              />
+            </v-list-item-action>
+          </v-list-item>
+          <v-list-item @click="logout">
+            <v-list-item-title color="warning">
+              <span style="color: #ff5252;">
+                ログアウト
+              </span>
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-main>
       <nuxt />
@@ -85,6 +122,21 @@ export default Vue.extend({
       miniVariant: false,
       right: true,
       rightDrawer: false
+    }
+  },
+  computed: {
+    avatarText () {
+      return this.$store.state.LoginUser.name.substr(0, 2)
+    },
+    user () {
+      return this.$store.state.LoginUser
+    }
+  },
+  methods: {
+    logout () {
+      this.$store.commit('LoginUser/logout')
+      this.$store.commit('Token/removeAccessToken')
+      this.$router.push('/login')
     }
   }
 })
